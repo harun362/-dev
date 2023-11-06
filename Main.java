025a7b955a01bc4e459a608c26436ffb35169411
harun362 <149924286+harun362@users.noreply.github.com>
2023-11-06 23:59:51 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int yas = 19;
+        String ad = "harun";
+        String soyad = "demir";
+        String sinifi = "bilgisayar teknolojisi";
+        String memleket = "samsun";
+
+        System.out.println(yas);
+        System.out.println(ad);
+        System.out.println(soyad);
+        System.out.println(sinifi);
+        System.out.println(memleket);
+      }         
+
+
+     
+
+
+
+
+  }
